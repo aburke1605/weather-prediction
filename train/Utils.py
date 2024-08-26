67991bd6934c9b7fd55e47f02597d4d6e2d036d3
aburke1605 <aodhanburke@hotmail.com>
2024-08-26 12:05:29 +0100

    function to plot variable on map

--- train/Utils.py
+++ train/Utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xarray as xr
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def Open(filename, variable) -> pd.DataFrame:
     ds = xr.open_dataset(filename)
@@ -19,3 +20,23 @@ def Open(filename, variable) -> pd.DataFrame:
     df[variable] = (df[variable] + mini) / (mini + maxi)
 
     return df
+
+
+
+def Plot(latitude, longitude, variable, label=None, difference=False):
+    """
+    x = r λ cos(φ0)
+    y = r φ
+
+    r = radius of earth
+    """
+    r = 6271 # km
+    phi_0 = (min(latitude) + max(latitude)) / 2
+
+    x = r * longitude * np.cos(phi_0)
+    y = r * latitude
+
+    fig, ax = plt.subplots(figsize=(6,10))
+    _, _, _, im = ax.hist2d(x, y, weights=variable, bins=150, cmap="coolwarm" if difference else "Blues")
+    fig.colorbar(im, label=label)
+    plt.show()
