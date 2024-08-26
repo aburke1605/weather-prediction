c0a493b45c2abde0da1cc2b4dc52c29b6bc1be6c
aburke1605 <aodhanburke@hotmail.com>
2024-08-26 12:17:20 +0100

    read things

--- train/__main__.py
+++ train/__main__.py
@@ -1,9 +1,21 @@
 from .Utils import Open
 
+import pandas as pd
+from functools import reduce
+
 
 def main():
-    Open("./data/wind_speed/sfcWind_hadukgrid_uk_12km_mon_201001-201012.nc", "sfcWind")
+    rainfall = Open("data/rainfall/rainfall_hadukgrid_uk_12km_mon_201001-201012.nc", "rainfall")
+    sun_duration = Open("data/sun_duration/sun_hadukgrid_uk_12km_mon_201001-201012.nc", "sun")
+    wind_speed = Open("data/wind_speed/sfcWind_hadukgrid_uk_12km_mon_201001-201012.nc", "sfcWind")
 
 
-if __file__ == "__main__":
+    data_frames = [rainfall, sun_duration, wind_speed]
+    df_merged = reduce(lambda  left,right: pd.merge(left,right,on=["month_number", "latitude", "longitude"], how="outer"), data_frames)
+    df_merged.dropna(inplace=True)
+    print(df_merged.shape)
+    print(df_merged.head())
+
+
+if __name__ == "__main__":
     main()
