6b737fe78aa45605710e53f0416bfd40462ba661
aburke1605 <aodhanburke@hotmail.com>
2024-08-26 13:13:24 +0100

    make reading a function

--- train/Utils.py
+++ train/Utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import xarray as xr
 import pandas as pd
 import matplotlib.pyplot as plt
+from functools import reduce
 
 def Open(filename:str, variable:str, rename_variable:str|None=None) -> pd.DataFrame:
     ds = xr.open_dataset(filename)
@@ -27,6 +28,25 @@ def Open(filename:str, variable:str, rename_variable:str|None=None) -> pd.DataFr
 
     return df
 
+def ReadYear(year:int) -> pd.DataFrame:
+    data_frames = []
+    for variable, new_variable in {
+        "hurs":     "humidity",
+        "tasmax":   "max_temperature",
+        "tas":      "mean_temperature",
+        "tasmin":   "min_temperature",
+        "psl":      "pressure",
+        "rainfall": "rainfall",
+        "sun":      "sun_duration",
+        "sfcWind":  "wind_speed",
+    }.items():
+        data_frames.append(Open(f"data/{new_variable}/{variable}_hadukgrid_uk_12km_mon_{year}01-{year}12.nc", variable, rename_variable=new_variable))
+
+    data = reduce(lambda  left,right: pd.merge(left,right,on=["month_number", "latitude", "longitude"], how="outer"), data_frames)
+    data.dropna(inplace=True)
+    data.reset_index(drop=True, inplace=True)
+
+    return data
 
 
 def Plot(latitude, longitude, variable, label=None, difference=False):
