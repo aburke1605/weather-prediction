99a4ded33b1ce88b9485661ab8d09da2b941102e
aburke1605 <aodhanburke@hotmail.com>
2024-08-26 12:30:44 +0100

    read others

--- train/__main__.py
+++ train/__main__.py
@@ -8,15 +8,21 @@ def main():
 
     data_frames = []
     for variable, new_variable in {
+        "hurs":     "humidity",
+        "tasmax":   "max_temperature",
+        "tas":      "mean_temperature",
+        "tasmin":   "min_temperature",
+        "psl":      "pressure",
         "rainfall": "rainfall",
-        "sun": "sun_duration",
-        "sfcWind": "wind_speed",
+        "sun":      "sun_duration",
+        "sfcWind":  "wind_speed",
     }.items():
         data_frames.append(Open(f"data/{new_variable}/{variable}_hadukgrid_uk_12km_mon_201001-201012.nc", variable, rename_variable=new_variable))
 
-    df_merged = reduce(lambda  left,right: pd.merge(left,right,on=["month_number", "latitude", "longitude"], how="outer"), data_frames)
-    df_merged.dropna(inplace=True)
-
+    data = reduce(lambda  left,right: pd.merge(left,right,on=["month_number", "latitude", "longitude"], how="outer"), data_frames)
+    data.dropna(inplace=True)
+    print(data.shape)
+    print(data.head())
 
 if __name__ == "__main__":
     main()
